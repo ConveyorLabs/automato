cd955fa88291642b90701a30d22dd1dd8ffc1b61
lamachinefrancaise <lamachinefrancaise@proton.me>
2022-06-25 23:09:26 -0400

    added wallet file

---
+++ wallet/wallet.go
@@ -0,0 +1,165 @@
+package wallet
+
+import (
+	rpcClient "automato/rpc_client"
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"golang.org/x/crypto/sha3"
+)
+
+var Wallet EOA
+
+type EOA struct {
+	SignerAddress      common.Address
+	Signer             types.Signer
+	PrivateKey         *ecdsa.PrivateKey
+	OniContractAddress *common.Address
+}
+
+func InitializeEOAFromConfig() {
+
+	wallet := os.Getenv("WALLET_ADDRESS")
+	wallet, err := toChecksumAddress(wallet)
+
+	private_key := os.Getenv("PRIVATE_KEY")
+
+	pk := initializePrivateKey(wallet, private_key)
+
+	if err != nil {
+		panic("To checksumAddress failed, use a correct PublicKey")
+	}
+
+	chainId := os.Getenv("CHAIN_ID")
+
+	newBigInt := new(big.Int)
+	chainIdBigInt, ok := newBigInt.SetString(chainId, 0)
+
+	if !ok {
+		fmt.Println("Error when converting string to big int during chainId initialization")
+		return
+	}
+
+	Wallet = EOA{
+		SignerAddress: common.HexToAddress(wallet),
+		Signer:        types.LatestSignerForChainID(chainIdBigInt),
+		PrivateKey:    pk,
+	}
+
+}
+
+//Initialize a new private key and wipe the input after usage
+func initializePrivateKey(walletChecksumAddress string, privateKey string) *ecdsa.PrivateKey {
+	//Initialize a key variable
+	var walletKey *ecdsa.PrivateKey
+
+	if privateKey[:2] == "0x" {
+		privateKey = privateKey[2:]
+	}
+
+	ecdsaPrivateKey, err := crypto.HexToECDSA(privateKey)
+
+	if err != nil {
+		errString := fmt.Sprintf("Incorrect or invalid private key for %s. Please check your wallet address/private key and try again.\n", walletChecksumAddress)
+		panic(errString)
+	} else {
+		//Set user wallet private key
+		walletKey = ecdsaPrivateKey
+	}
+	// Return the wallet key
+	return walletKey
+}
+
+//Convert a hex address to checksum address
+func toChecksumAddress(address string) (string, error) {
+
+	//Check that the address is a valid Ethereum address
+	re1 := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	if !re1.MatchString(address) {
+		return "", fmt.Errorf("given address '%s' is not a valid Ethereum Address", address)
+	}
+
+	//Convert the address to lowercase
+	re2 := regexp.MustCompile("^0x")
+	address = re2.ReplaceAllString(address, "")
+	address = strings.ToLower(address)
+
+	//Convert address to sha3 hash
+	hasher := sha3.NewLegacyKeccak256()
+	hasher.Write([]byte(address))
+	sum := hasher.Sum(nil)
+	addressHash := fmt.Sprintf("%x", sum)
+	addressHash = re2.ReplaceAllString(addressHash, "")
+
+	//Compile checksum address
+	checksumAddress := "0x"
+	for i := 0; i < len(address); i++ {
+		indexedValue, err := strconv.ParseInt(string(rune(addressHash[i])), 16, 32)
+		if err != nil {
+			fmt.Println("Error when parsing addressHash during checksum conversion", err)
+			return "", err
+		}
+		if indexedValue > 7 {
+			checksumAddress += strings.ToUpper(string(address[i]))
+		} else {
+			checksumAddress += string(address[i])
+		}
+	}
+
+	//Return the checksummed address
+	return checksumAddress, nil
+}
+
+func signAndSendTx(toAddress *common.Address, calldata []byte, msgValue *big.Int, gas uint64, gasTipCap *big.Int, gasFeeCap *big.Int) {
+
+	//get wallet nonce
+	nonce, err := rpcClient.HTTPClient.NonceAt(context.Background(), Wallet.SignerAddress, nil)
+	if err != nil {
+		fmt.Println(err)
+		//TODO: In the future, handle errors gracefully
+		os.Exit(1)
+	}
+
+	// //initialize txdata
+	// txData, err := OniABI.Pack("arb_market", marketId, lpAddress, loanAmount)
+	// if err != nil {
+	// 	//TODO: how to handle runtime errors
+	// 	fmt.Println(err)
+	// }
+
+	tx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   Wallet.Signer.ChainID(),
+		Nonce:     nonce,
+		GasFeeCap: gasFeeCap,
+		GasTipCap: gasTipCap,
+		Gas:       gas,
+		To:        toAddress,
+		Value:     msgValue,
+		Data:      calldata,
+	})
+
+	signedTx, err := types.SignTx(tx, Wallet.Signer, Wallet.PrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		//TODO: In the future, handle errors gracefully
+		os.Exit(1)
+	}
+
+	//send the transaction
+	txErr := rpcClient.HTTPClient.SendTransaction(context.Background(), signedTx)
+	if txErr != nil {
+		fmt.Println(err)
+		//TODO: In the future, handle errors gracefully
+		os.Exit(1)
+	}
+
+}
